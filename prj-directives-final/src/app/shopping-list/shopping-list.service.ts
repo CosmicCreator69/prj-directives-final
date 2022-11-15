1e81d5e177ab3f27a831e81715b0bd644107121c
CosmicCreator69 <112199101+CosmicCreator69@users.noreply.github.com>
2022-11-15 14:11:59 -0600

    Using Services for Pushing Data from A to B

--- prj-directives-final/src/app/shopping-list/shopping-list.service.ts
+++ prj-directives-final/src/app/shopping-list/shopping-list.service.ts
@@ -1,6 +1,9 @@
+import EventEmitter = require("events");
+import { TouchSequence } from "selenium-webdriver";
 import { Ingredient } from "../shared/ingredient.model";
 
 export class ShoppingListService{
+  ingredientsChanged= new EventEmitter<Ingredient[]>();
   private ingredients: Ingredient[] = [
     new Ingredient('Apples', 5),
     new Ingredient('Tomatoes', 10),
@@ -10,6 +13,7 @@ getIngredients(){
 }
 addIngredient(ingredient:Ingredient){
   this.ingredients.push(ingredient)
+  this.ingredientsChanged.emit(this.ingredients.slice());
 
   }
 }
